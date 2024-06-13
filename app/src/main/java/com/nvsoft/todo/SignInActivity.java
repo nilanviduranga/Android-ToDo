05cb8eca3a94d1e4a1794f5c4dc94346fbf6ebd3
Nilan Viduranga <nilan.v@parallax.lk>
2024-06-14 00:02:21 +0530

    sign up and sign in be
    sign up and sign in be

--- app/src/main/java/com/nvsoft/todo/SignInActivity.java
+++ app/src/main/java/com/nvsoft/todo/SignInActivity.java
@@ -5,23 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
     TextView signUp;
+    EditText userEmail;
+    EditText userPassword;
+    Button signInButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
         signUp = findViewById(R.id.sign_up_text);
+        userEmail = findViewById(R.id.user_email_sign_in);
+        userPassword = findViewById(R.id.user_password_sign_in);
+        signInButton = findViewById(R.id.button);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = userEmail.getText().toString();
+                String password = userPassword.getText().toString();
+                if (UserManager.validateUser(SignInActivity.this, email, password)) {
+                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(SignInActivity.this, "Wrong email or password", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/nvsoft/todo/SignUpActivity.java
+++ app/src/main/java/com/nvsoft/todo/SignUpActivity.java
@@ -5,23 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
 
     TextView signIn;
+    EditText userEmail;
+    EditText password;
+    EditText confirmPassword;
+    Button signUpButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
         signIn = findViewById(R.id.sign_in_text);
+        userEmail = findViewById(R.id.user_email);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirm_password);
+        signUpButton = findViewById(R.id.sign_up_button);
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = userEmail.getText().toString();
+                String pwd = password.getText().toString();
+                String confirmPwd = confirmPassword.getText().toString();
+
+                if (pwd.equals(confirmPwd)) {
+                    if (UserManager.addUser(SignUpActivity.this, email, pwd)) {
+                        Toast.makeText(SignUpActivity.this, "User Registered Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "User already exists", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
